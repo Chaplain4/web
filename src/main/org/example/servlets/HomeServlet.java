459281f7ef4f2441fd27e3589971ec037be25069
ITSTEP\st <auske_sz16@student.itstep.by>
2023-10-25 21:27:51 +0300

    Classwork commited

--- src/main/org/example/servlets/HomeServlet.java
+++ src/main/org/example/servlets/HomeServlet.java
@@ -1,20 +1,18 @@
-package main.org.example;
+package main.org.example.servlets;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hi from Servlet");
+        System.out.println("Server time : " + new Date());
+        resp.getWriter().println("Hi there, this is Servlets response. " + new Date());
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
 }
