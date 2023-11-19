27ed59031936938d23dc41a9fce0cfb003db5fbe
Arthur Auskern <Chaplain04@gmail.com>
2023-11-19 21:15:45 +0300

    no message

--- src/main/homework/tester.java
+++ src/main/homework/tester.java
@@ -9,10 +9,24 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class tester {
     public static void main(String[] args) {
-        System.out.println(IOUtils.readFile("src/main/webapp/users.html"));
+        DogDAOImpl ddi = new DogDAOImpl();
+        Set<Dog> dogs = ddi.all();
+        List<Dog> dogs1 = new ArrayList<>(dogs);
+        HTMLTableBuilder htmlTableBuilder = new HTMLTableBuilder("all the dogs in db", true, dogs1.size(), 4, 5, 5, 5);
+        htmlTableBuilder.addTableHeader("id", "age", "name", "breed");
+        for (int i = dogs1.size(); i > 0; i--) {
+            htmlTableBuilder.addRowValues(String.valueOf(dogs1.get(i - 1).getId()), String.valueOf(dogs1.get(i - 1).getAge()), dogs1.get(i - 1).getName(), dogs1.get(i - 1).getBreed());
+        }
+        String str = IOUtils.readFile("C:\\Users\\admin\\Documents\\1'web\\src\\main\\webapp\\manager.html");
+        String str2 = htmlTableBuilder.build();
+        System.out.println(str.replace("123456", str2));
+        //IOUtils.write(str, "C:\\Users\\admin\\Documents\\1'web\\src\\main\\webapp\\manager.html");
     }
 }
 
--- src/main/org/example/servlets/ManagerServlet.java
+++ src/main/org/example/servlets/ManagerServlet.java
@@ -20,29 +20,41 @@ import static main.org.example.util.ServletUtils.forward;
 
 @WebServlet("/manager")
 public class ManagerServlet extends HttpServlet {
-
+    DogDAOImpl ddi = new DogDAOImpl();
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        Set<Dog> dogs = ddi.all();
+        List<Dog> dogs1 = new ArrayList<>(dogs);
+        HTMLTableBuilder htmlTableBuilder = new HTMLTableBuilder("all the dogs in db13", true, dogs1.size(), 4, 5, 5, 5);
+        htmlTableBuilder.addTableHeader("id", "age", "name", "breed");
+        for (int i = dogs1.size(); i > 0; i--) {
+            htmlTableBuilder.addRowValues(String.valueOf(dogs1.get(i - 1).getId()), String.valueOf(dogs1.get(i - 1).getAge()), dogs1.get(i - 1).getName(), dogs1.get(i - 1).getBreed());
+        }
+        String str = IOUtils.readFile("C:\\Users\\admin\\Documents\\1'web\\src\\main\\webapp\\manager.html");
+        String str2 = htmlTableBuilder.build();
+        String str3 = (str.replace("123456", str2));
+        PrintWriter pw = resp.getWriter();
+        pw.println(str3);
         forward(req, resp, "/manager.html"); //move forward with the same params and so on
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        DogDAOImpl ddi = new DogDAOImpl();
+
         Set<Dog> dogs = ddi.all();
         List<Dog> dogs1 = new ArrayList<>(dogs);
-        HTMLTableBuilder htmlTableBuilder = new HTMLTableBuilder("all the dogs in db", true, dogs1.size(), 4, 5, 5, 5);
+        HTMLTableBuilder htmlTableBuilder = new HTMLTableBuilder("all the dogs in db13", true, dogs1.size(), 4, 5, 5, 5);
         htmlTableBuilder.addTableHeader("id", "age", "name", "breed");
         for (int i = dogs1.size(); i > 0; i--) {
             htmlTableBuilder.addRowValues(String.valueOf(dogs1.get(i - 1).getId()), String.valueOf(dogs1.get(i - 1).getAge()), dogs1.get(i - 1).getName(), dogs1.get(i - 1).getBreed());
         }
         String str = IOUtils.readFile("C:\\Users\\admin\\Documents\\1'web\\src\\main\\webapp\\manager.html");
-        str.replace("PLACEHOLDER", htmlTableBuilder.build());
-        IOUtils.write(str, "C:\\Users\\admin\\Documents\\1'web\\src\\main\\webapp\\manager.html");
+        String str2 = htmlTableBuilder.build();
+        String str3 = (str.replace("123456", str2));
         PrintWriter pw = resp.getWriter();
-        String table = htmlTableBuilder.build();
-        resp.setContentType("text/html");
-        pw.println(table);
+        pw.println(str3);
     }
 }
 
